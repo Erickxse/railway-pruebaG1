b9baee46e35052cf48f656935cef64cc3bf64a66
ErickxSebastianMora <erick_sebastian_@hotmail.com>
2024-06-06 22:30:15 -0500

    QUotes

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, jsonify
 import random
 import os
 
 app = Flask(__name__)
-
-# Usuarios y contraseñas de ejemplo (deberías cambiar esto en un entorno real)
-usuarios = {
-    "usuario1": "contraseña1",
-    "usuario2": "contraseña2"
-}
 
 quotes = [
     "La mejor forma de predecir el futuro es creandolo. - Peter Drucker",
@@ -30,24 +24,8 @@ quotes = [
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        quote = random.choice(quotes)
-        return jsonify({"quote": quote})
-    else:
-        return redirect(url_for('login'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username in usuarios and usuarios[username] == password:
-            session['username'] = username
-            return redirect(url_for('index'))
-        else:
-            return render_template('login.html', message='Credenciales inválidas. Por favor, inténtalo de nuevo.')
-    return render_template('login.html')
+    quote = random.choice(quotes)
+    return jsonify({"quote": quote})
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'  # Clave secreta para la sesión (deberías cambiar esto en un entorno real)
     app.run(debug=True, port=os.getenv("PORT", default=5000))
