4ff469cc058622d380d5ed90dd2452f2f053ed33
ErickxSebastianMora <erick_sebastian_@hotmail.com>
2024-06-06 22:10:36 -0500

    login test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("login.html")
-database={'nachi':'123','james':'aac','karthik':'asdsf'}
+database={'erickxse':'123','james':'aac','karthik':'asdsf'}
 
 @app.route('/form_login',methods=['POST','GET'])
 def login():
